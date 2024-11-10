5d9f860a6a5eb7ae7bf1805ba2c8da7379c0a5ec
nallacheruvus <41948894+nallacheruvus@users.noreply.github.com>
2024-11-10 15:36:11 +0530

    My First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Satish from '/Satish.png'
+
+function App() {
+  return (
+    <>
+      <center>
+        <img src={Satish} />
+      </center>
+      <h1 style={{ color: 'white' }}>Satish Welcomes You To His React Web Site!</h1>
+    </>
+  )
+}
+
+export default App
